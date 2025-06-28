0ee7d45a03ca0d0683f9dad69eb6d4c9687067e5
Matheus Valpassos <matvalpassos@gmail.com>
2025-06-27 23:39:33 -0300

    Remove outdated comment regarding production database configuration in settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -55,7 +55,6 @@ TEMPLATES = [
     },
 ]
 
-# Configuração do Banco de Dados para Produção (Supabase)
 DATABASES = {
     "default": dj_database_url.config(
         default=config("DATABASE_URL"), conn_max_age=600, ssl_require=True
